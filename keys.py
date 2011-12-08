0076cd8dbf81f5b970a55db8f770c956202e617b
Colin Davis <cdavis@tavern.is>
2011-12-08 18:49:49 -0500

    Encryption re-implemented.

--- keys.py
+++ keys.py
@@ -1,52 +1,86 @@
 import os,re
-import string
-
- #Stub this out for now.
+import string,hashlib,base64
+import Crypto.PublicKey.RSA as RSA
+import Crypto.Hash.MD5 as MD5
  
 class Keys(object):
-    def empty_callback ():
-        return
 
-    def __init__(self,pub=None,priv=None):
-        #The point of doing the split, and the replaces, below, is to ensure we have no whitespace in the public keys.
-        #Just the straight key, as a long string. No \r or \n
-        self.Keys = "ff"
-        self.pubkey = "Pub"
-        self.privkey = "priv"
+    def __init__(self,pub=None,priv=None):    
+        self.pubkey = pub
+        self.privkey = priv
         
+        if priv == None and pub != None:
+            self.key = RSA.importKey(pub)
+            
+        if priv != None and pub != None:
+            combinedkey = pub + priv
+            self.key = RSA.importKey(combinedkey)
+        
+        if priv != None and pub == None:
+            print("Weirdness. Don't do that.")
+            return False   
+            
+            
+             
     def formatkeys(self):
-     #Format the priv and pub keys
-     #Make sure we have proper linebreaks every 64 characters, and a header/footer
+        #Format the priv and pub keys
+        #Make sure we have proper linebreaks every 64 characters, and a header/footer
+        
+        #Strip out the headers
+        #Strip out the linebreaks
+        #Re-Add the Linebreaks
+        #Re-add the headers
+        
+        #Check for compressed versions-
+        if self.privkey is not None:
+            self.privkey = self.privkey.replace("-----BEGINRSAPRIVATEKEY-----","-----BEGIN RSA PRIVATE KEY-----")
+            self.privkey = self.privkey.replace("-----ENDRSAPRIVATE KEY-----","-----END RSA PRIVATE KEY-----")
 
-     #Strip out the headers
-     #Strip out the linebreaks
-     #Re-Add the Linebreaks
-     #Re-add the headers
-     print("foo")
-
+        if self.pubkey is not None:
+            self.pubkey = self.pubkey.replace("-----BEGINPUBLICKEY-----","-----BEGIN PUBLIC KEY-----")
+            self.pubkey = self.pubkey.replace("-----ENDPUBLICKEY-----","-----END PUBLIC KEY-----")
+            
+        if self.privkey is not None:
+            if "-----BEGIN RSA PRIVATE KEY-----" in self.privkey:
+                noHeaders=self.privkey[self.privkey.find("-----BEGIN RSA PRIVATE KEY-----")+31:self.privkey.find("-----END RSA PRIVATE KEY-----")]
+            else:
+                print("USING NO HEADER VERSION OF PRIVKEY")
+                noHeaders = self.privkey
+            noBreaks = "".join(noHeaders.split())
+            withLinebreaks = "\n".join(re.findall("(?s).{,64}", noBreaks))[:-1]            
+            self.privkey = "-----BEGIN RSA PRIVATE KEY-----\n" + withLinebreaks + "\n-----END RSA PRIVATE KEY-----"        
+        else:
+            print("No PRIVKEY")
+            
+        if self.pubkey is not None:
+            if "-----BEGIN PUBLIC KEY-----" in self.pubkey:
+                noHeaders=self.pubkey[self.pubkey.find("-----BEGIN PUBLIC KEY-----")+26:self.pubkey.find("-----END PUBLIC KEY-----")]
+            else:
+                print("USING NO HEADER VERSION OF PUBKEY")
+                noHeaders=self.pubkey
+            noBreaks = "".join(noHeaders.split())
+            withLinebreaks = "\n".join(re.findall("(?s).{,64}", noBreaks))[:-1]
+            self.pubkey = "-----BEGIN PUBLIC KEY-----\n" + withLinebreaks + "\n-----END PUBLIC KEY-----" 
 
 
     def generate(self):
-
-        #TODO- use __get_addr__ to calc these OnDemand
-        self.privkey = "Pub"      
-        self.pubkey = "priv" 
-
-        self.combinedkey = self.privkey + '\n' + self.pubkey
-        self.combinedkey = self.combinedkey.encode('utf8')    
-        self.formatkeys()
+        self.key = RSA.generate(4096)
+        self.pubkey = str(self.key.publickey().exportKey(),encoding='latin1')
+        self.privkey  = str(self.key.exportKey(),encoding='latin1')
 
     def signstring(self,signstring):
-        return "StringSignature"
+        
+        # Second Parameter is not needed for RSA.        
+        return self.key.sign(signstring.encode('utf-8'),'')
 
     def verifystring(self,stringtoverify,signature):
-        return True
+        return self.key.verify(stringtoverify.encode('utf-8'),signature)
 
     def encryptToSelf(self,encryptstring):
-        return "true"
+        return base64.b64encode(self.key.encrypt(encryptstring.encode('utf-8'),'')[0]).decode('utf-8')
 
     def decryptToSelf(self,decryptstring):
-        return "true"
+        return self.key.decrypt(base64.b64decode(decryptstring.encode('utf-8'))).decode('utf-8')
 
     def dokeysmatch(self):
         self.formatkeys()
