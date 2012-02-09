58500890e4efc1e529d2606fa0e7f6c3b65d379a
Colin Davis <cdavis@tavern.is>
2012-02-09 02:08:18 -0500

    rename for case

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 kill `ps aux | grep [w]ebfront | awk {'print $2'}`; nohup ./webfront.py &
 kill `ps aux | grep [a]pi | awk {'print $2'}`; nohup ./api.py &
 ./TopicList.py
-./Modlist.py
+./ModList.py
 ./DiskTopics.py
