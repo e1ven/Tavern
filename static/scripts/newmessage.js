a563c6b66e0482430b9edd449da7187d8f2866b3
Colin Davis <cdavis@tavern.is>
2012-05-21 22:13:27 -0400

    Add missing files

---
+++ static/scripts/newmessage.js
@@ -0,0 +1,4 @@
+$("#nonjs_files_list").css("display", "none");
+var multi_selector = new MultiSelector( document.getElementById('js_files_list' ), 5 );
+multi_selector.addElement( document.getElementById( 'first_upload_element' ) );
+
