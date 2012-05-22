f0731f59d88f096e9f554e24e031d77ca2fe6431
Colin Davis <cdavis@tavern.is>
2012-05-22 11:23:09 -0400

    Version with HTML textbar

---
+++ static/scripts/simple.js
@@ -0,0 +1,22 @@
+var wysihtml5ParserRules = {
+  tags: {
+    strong: {},
+    b:      {},
+    i:      {},
+    em:     {},
+    br:     {},
+    p:      {},
+    ul:     {},
+    ol:     {},
+    li:     {},
+    a:      {
+      set_attributes: {
+        target: "_blank",
+        rel:    "nofollow"
+      },
+      check_attributes: {
+        href:   "url" // important to avoid XSS
+      }
+    }
+  }
+};
