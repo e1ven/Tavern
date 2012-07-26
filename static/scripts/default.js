eaf3c38b38df66082af09199ae2a6a77be3f7d95
Colin Davis <cdavis@tavern.is>
2012-07-26 19:29:10 -0400

    Re-add feature to save positions of the sliders, in the new slider system

--- static/scripts/default.js
+++ static/scripts/default.js
@@ -1,16 +1,52 @@
 
 jQuery().ready(function() {
     $jQuery = jQuery.noConflict();
-    var currentelement = "";
 
     if (jQuery("#centerandright").length)
     {
 
+        // If we've saved any values to storage, retrieve them, and keep the page the size it was.
+
+        left = jQuery.jStorage.get('#left.width','');
+        centerandright = jQuery.jStorage.get('#centerandright.width','');
+        center = jQuery.jStorage.get('#center.width','');
+        right =  jQuery.jStorage.get('#right.width','');
+
+        if (left + centerandright + center + right != '' )
+        {
+            jQuery('#left').css('width',left);
+            jQuery('#centerantright').css('width',centerandright);
+            jQuery('#center').css('width',center);
+            jQuery('#right').css('width',right);
+
+        }
+        else
+        {
+            //default widths
+            jQuery('#left').width(jQuery('#left').parent().parent().width() * .15)
+            jQuery('#centerandright').width(jQuery('#centerandright').parent().parent().width() * .85)
+            jQuery('#center').width(jQuery('#centerandright').width() * .25)
+            jQuery('#right').width(jQuery('#centerandright').width() * .75)
+        }
+        
+        // Now, do the splitting.
         VerticalSplitter.SetUpElement({ containerId: "content", firstItemId: "left", secondItemId: "centerandright" });
         VerticalSplitter.SetUpElement({ containerId: "centerandright", firstItemId: "center", secondItemId: "right" });
     }
-    // Ensure that if we move Splitter 1 and push splitter2 offscreen, we still redraw it.
+
+
+
     jQuery('.splitter').mouseup(function() {
+
+        // On Mouseup, save the current position, so we can do new pages at the same place.
+
+        jQuery.jStorage.set('#left.width',jQuery('#left').css('width'));
+        jQuery.jStorage.set('#centerandright.width',jQuery('#centerandright').css('width'));
+        jQuery.jStorage.set('#center.width',jQuery('#center').css('width'));
+        jQuery.jStorage.set('#right.width',jQuery('#right').css('width'));
+
+        // Redraw the splitter, so ensure that if we move Splitter 1 and push splitter2 offscreen, we still redraw it.
+
         VerticalSplitter.SetUpElement({ containerId: "centerandright", firstItemId: "center", secondItemId: "right" });
     });
 
