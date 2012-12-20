2ffac25b1fe01aca836689a7035bbf8f55b1c083
Colin Davis <cdavis@tavern.is>
2012-12-20 01:22:17 -0500

    Fix Mailcheck

--- static/scripts/register.js
+++ static/scripts/register.js
@@ -1,30 +1,44 @@
-jQuery(document).ready(function() {
-	jQuery('#email').blur(function() {
-	    jQuery(this).mailcheck({
-	      suggested: function(element, suggestion) {
-	        jQuery('#EmailStatus').html('<p><i>&nbsp;&nbsp;&nbsp; Did you mean '+suggestion.address+'@'+'<strong>'+suggestion.domain+'</strong>?</i></p>').show();
+jQuery(document).ready(function() 
+{
 
-	        jQuery('#EmailStatus').click(function(event) {
-	        	jQuery('#email').val(suggestion.full);
-	        	jQuery('#EmailStatus').hide();
-	        });
-	      
 
-	      },
-	      empty: function(element) {
-	        jQuery('#EmailStatus').html('').hide();
-	      }
-	    });
+
+
+	// Calculate the domain they MIGHT have meant.
+	jQuery('#email').on('blur', function() 
+	{
+	  Kicksend.mailcheck.run(
+	  {
+		  email: jQuery('#email').val(),
+		  suggested: function(suggestion) 
+		  {
+		  	jQuery('#EmailStatus').html('<p><i>&nbsp;&nbsp;&nbsp; Did you mean '+suggestion.address+'@'+'<strong>'+suggestion.domain+'</strong>?</i></p>').show();
+
+		    //Register a handler to hide this new suggestion.
+		    jQuery('#EmailStatus').click(function(event) 
+		    {
+		    	jQuery('#email').val(suggestion.full);
+		    	jQuery('#EmailStatus').hide();    
+		    });
+		   },
+		    empty: function(element) 
+		    {
+			        jQuery('#EmailStatus').html('').hide();
+		    }
+	   });
 	});
 
 
-	jQuery('#pass').keyup(function() {
+
+	jQuery('#pass').keyup(function() 
+	{
 		result = zxcvbn( this.value );
 		pass1 = this.value
 		PassDescriptions = ['Not Very Strong', 'Average', 'Good', 'Very Good', 'Great'];
 		jQuery('#PasswordStatus').html("<p><i>&nbsp;&nbsp;&nbsp;Password Strength: " + PassDescriptions[result.score]) + "</i></p>";
 	});
-	jQuery('#pass2').keyup(function() {
+	jQuery('#pass2').keyup(function() 
+	{
 		if (this.value != pass1 )
 		{
 			jQuery('#PasswordStatus').html("<p><i>&nbsp;&nbsp;&nbsp;Passwords do not match</i></p>");
@@ -34,10 +48,4 @@ jQuery(document).ready(function() {
 			jQuery('#PasswordStatus').html("<p><i>&nbsp;&nbsp;&nbsp;Passwords match</i></p>");
 		}
 	});
-
-
-
-
-
 });
-
