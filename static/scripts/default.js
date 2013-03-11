65dfda73cfc5e39b25c2271f803975828b27b3c4
Colin Davis <cdavis@tavern.is>
2013-03-11 07:36:47 -0400

    CSS

--- static/scripts/default.js
+++ static/scripts/default.js
@@ -376,6 +376,30 @@ jQuery(document).ready(function() {
 
 
 
+        // Built the Javascript accordian menu.
+
+        // Hide the sub-menus via JAVASCRIPT, so they are there if JS is disabled.
+        jQuery(".accordion li > .sub-menu").css("display","none");
+        // Store variables
+        var accordion_head = $('.accordion > li > a'),
+            accordion_body = $('.accordion li > .sub-menu');
+        // Open the first tab on load
+        accordion_head.first().addClass('active').next().slideDown('normal');
+        // Click function
+        accordion_head.on('click', function(event) {
+            // Disable header links
+            event.preventDefault();
+            // Show and hide the tabs on click
+            if ($(this).attr('class') != 'active'){
+                accordion_body.slideUp('normal');
+                $(this).next().stop(true,true).slideToggle('normal');
+                accordion_head.removeClass('active');
+                $(this).addClass('active');
+            }
+        });
+  
+
+
 
 // Run the per-instance stuff.
 jQuery.getScript('/static/scripts/instance.min.js');
